487a246692bb98f9b094ff0510ede79276858fac
Valeriy Eremin <r.stream.dn@gmail.com>
2016-08-08 16:20:20 +0300

    stalker2

--- php/mail.php
+++ php/mail.php
@@ -11,27 +11,12 @@ $subject = $_POST['formData'];//тема письма
 $message = "<b>".$_POST['formData']."</b><br>\r\n";
 $message .= "<b>ФИО :</b> ".$_POST['name']."<br>\r\n";
 $message .= "<b>Телефон:</b> ".$_POST['phone']."<br>\r\n";
-$message .= "<b>Регион проживания:</b> ".$_POST['address']."<br>\r\n";
-$message .= "<b>Дата рождения:</b> ".$_POST['birthday']."<br>\r\n";
+
 $message .= "<b>Почта:</b> ".$_POST['email']."<br>\r\n";
-$message .= "<b>Паспорт:</b> ".$_POST['series']." ".$_POST['number']."<br>\r\n";
-$message .= "<b>Код подразделения:</b> ".$_POST['code']."<br>\r\n";
-$message .= "<b>Кем выдан:</b> ".$_POST['who']."<br>\r\n";
-$message .= "<b>Адрес проживания:</b> ".$_POST['index-prop']." ".$_POST['address-prop']."<br>\r\n";
-$message .= "<b>Адрес проживания:</b> ".$_POST['index-live']." ".$_POST['address-live']."<br>\r\n";
 
-// хз правильно или нет с чекбоксом?!
-//$selectOption = $_POST['taskOption'];
-if(isset($_POST['checkbox']) && $_POST['checkbox'] == 'Yes')
-{
-    $message .= "<b>Адрес прописки совпадает с адресом проживания</b> ".$_POST['checkbox']."<br>\r\n";
-}
-else
-{
-    $message .= "<b>Адрес прописки НЕ совпадает с адресом проживания</b> ".$_POST['checkbox']."<br>\r\n";
-}
 
-if(!empty($_POST['question'])) {//туплю
+
+if(!empty($_POST['question'])) {
     $message .= "<b>Вопрос:</b> ".$_POST['question']."<br>\r\n";
 }
 
